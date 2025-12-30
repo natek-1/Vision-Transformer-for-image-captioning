81506c068788cee12fa8500378f6f93c60089608
natek-1 <nategabrielk@icloud.com>
2025-12-30 08:45:17 -0500

    updated file for dataset

--- vision/dataset/dataset.py
+++ vision/dataset/dataset.py
@@ -4,62 +4,45 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
+from transformers import AutoTokenizer
+
 from vision.tokenizer.vocab import Vocabulary
 
 
+
+
 class FlickrDataset(Dataset):
-    max_len = 45
-    def __init__(self, root_dir, data_dict, vocabulary: Vocabulary, transform=None, train=True):
+    max_len = 90
+    def __init__(self, root_dir, data_dict, transform=None):
         self.root_dir = root_dir
         self.data_dict = data_dict
         self.transform = transform
 
         # get the image and caption
-        self.train = train
-        self.caption = []
         self.item = self.setup_item()
 
-        # Create our own vocabulary
-        self.vocabulary = vocabulary
-        self.eos_token = vocabulary.tokenizer.eos_token
-        self.pad_token = vocabulary.tokenizer.pad_token
+        self.tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
     
     def __len__(self):
         return len(self.item)
     
     def setup_item(self):
         item = []
-        if self.train:
-            for image_id, image_captions in self.data_dict.items():
-                for caption in image_captions:
-                    item.append((image_id, caption))
-        else:
-            for image_id, image_captions in self.data_dict.items():
-                item.append((image_id, image_captions))
+        for image_id, image_captions in self.data_dict.items():
+            item.append((image_id, image_captions))
         return item
 
 
     def __getitem__(self, index):
         # get image
-        image_path = os.path.join(self.root_dir, self.item[index][0])
+        image_path = os.path.join(self.root_dir, self.item[index][0]) # path to image
         img = Image.open(image_path).convert('RGB')
 
         if self.transform is not None:
             img = self.transform(img)
         
         # get caption
-        caption = self.item[index][1]
-        
-        if self.train:
-            caption += self.eos_token
-            caption = self.vocabulary.encode(caption, True, True)
-            return img, torch.tensor(caption.input_ids, dtype=torch.long), torch.tensor(caption.attention_mask, dtype=torch.long)
-        else:
-            captions = torch.zeros(5, self.vocabulary.max_len).to(torch.long)
-            atten_mask = torch.zeros(5, self.vocabulary.max_len).to(torch.long)
-            for idx, cap in enumerate(caption):
-                cap += self.eos_token
-                cap = self.vocabulary.encode(cap,True, True)
-                captions[idx] = torch.tensor(cap.input_ids, dtype=torch.long)
-                atten_mask[idx] = torch.tensor(cap.attention_mask, dtype=torch.long)
-            return img, captions, atten_mask
+        caption = self.item[index][1] # list of relevant caption
+        num_caption = self.tokenizer(caption, add_special_tokens=True, max_length=FlickrDataset.max_len, padding='max_length', truncation=True,
+                                    return_tensors="pt")
+        return img, num_caption
